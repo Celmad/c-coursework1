afd2f7b34ee26263330165f18dc0c89143282a3c
manuel alaminos dominguez <manuel alaminos dominguez>
2017-10-10 12:37:07 +0100

    Add project files.

---
+++ BusinessObjects/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BusinessObjects
+{
+    public class Student
+    {
+        private int _matricNo;
+        private string firstName;
+        private string surname;
+        private int courseMark;
+        private int examMark;
+        private DateTime birtday;
+
+
+        public int Matric
+        {
+            get
+            {
+                return _matricNo;
+            }
+            set
+            {
+                _matricNo = value;
+            }
+        }
+
+        public void getMark()
+        {
+
+        }
+    }
+}
